5d271fa94200901d5f916b646366d8b9ea867268
demian <jongho.dev@gmail.com>
2022-05-29 05:25:44 +0900

    add 2753

---
+++ 2753/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * https://www.acmicpc.net/problem/2753
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int year;
+
+    cin >> year;
+    if (year % 4 == 0 && year % 100 != 0) {
+        cout << 1;
+    } else if (year % 400 == 0) {
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+}
